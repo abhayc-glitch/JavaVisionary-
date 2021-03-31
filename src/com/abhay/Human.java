9d027922b4a5f768f509d1d816c1977ac6de0fb7
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-03-31 10:13:20 -0700

    Constructors(human.java) and update to HackerRank solution.

---
+++ src/com/abhay/Human.java
@@ -0,0 +1,14 @@
+package com.abhay;
+
+public class Human{
+    //creating a constructor...
+    String name;
+    int age;
+    double weight;
+    Human(String name, int age, double weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        //the "THis" keyword to call another constructor in the same class
+    }
+}
